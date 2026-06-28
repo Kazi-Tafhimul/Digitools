1857898997acf502a03ac6f05689a8c30ca61959
Kazi-Tafhimul <tafhimul917@gmail.com>
2026-06-28 20:39:27 +0600

    navbar section done

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
+import { useState } from "react";
+import Navbar from './components/Navbar';
+
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [cartCount, setCartCount] = useState(0);
 
   return (
-    <>
-     
-     
-     
+    <div className="min-h-screen bg-white">
+      <Navbar cartCount={cartCount} />
 
-    
-
-      
-    </>
-  )
+      <div className="p-8 text-center">
+        <button
+          onClick={() => setCartCount(cartCount + 1)}
+          className="btn btn-outline"
+        >
+          Test Add Item (Cart: {cartCount})
+        </button>
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
